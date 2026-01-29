d98f694681bb3c8d59b403ff81657d13970069c7
Alex Fagan <196803660+IE-Jace@users.noreply.github.com>
2026-01-29 13:59:45 +0000

    defwreqwqrewklwfekorew

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,21 +13,27 @@ import { NgClass } from '@angular/common';
 export class App {
   protected readonly title = signal('Lab2');
 
-  protected movies: string[] = [
+  protected movies = signal<string[]>([
     'Fast and Furious 1',
     'Fast and Furious 2',
     'Fast and Furious 3',
     'Fast and Furious 4',
     'Fast and Furious 5',
-  ];
+  ]);
 
   protected favourites = signal<string[]>([]);
 
+  protected alertMessage()
+  {
+    alert('This is an alert message from the App component!');
+  }
   protected addToFavourites(movie: string) {
+    alert(`Added ${movie} to favourites`);
     this.favourites.update((current) => [...current, movie]);
   }
 
   protected deleteFavourite(index: number) {
+    alert(`Removed favourite at index ${index}`);
     this.favourites.update((current) => current.filter((_, i) => i !== index));
   }
 }
